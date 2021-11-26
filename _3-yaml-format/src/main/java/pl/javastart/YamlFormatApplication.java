eded0c8493f832fea34f6a20923e5b3fc9e61a5d
reszka303 <reszka303@op.pl>
2021-11-26 03:48:55 +0100

    3rd commit: Spring Boot - Introduction - Yaml format

---
+++ _3-yaml-format/src/main/java/pl/javastart/YamlFormatApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlFormatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlFormatApplication.class, args);
+    }
+
+}
