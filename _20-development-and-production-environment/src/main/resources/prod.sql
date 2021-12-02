d1e6aaf796a9e1cf85f0e5ec798f00c65989f847
reszka303 <reszka303@op.pl>
2021-12-02 05:35:00 +0100

    22nd commit: Profiles and schema Management - Development and Production Environment

---
+++ _20-development-and-production-environment/src/main/resources/prod.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE test DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci;
+
+INSERT INTO
+    product(name, price, description)
+VALUES
+    ('mleko', 3.2, 'świeże mleko od polskich krów'),
+    ('chleb', 3.8, 'zawsze świeży i chrupiący, po prostu idealny');
