10c4f715030141d8807b1a71d685cedbf006c6ac
reszka303 <reszka303@op.pl>
2021-12-05 07:21:25 +0100

    31st commit: Home page and static resources - Home page and static resources

---
+++ _29-home-page-and-static-resources/src/main/java/pl/javastart/hello/HelloController.java
@@ -0,0 +1,33 @@
+package pl.javastart.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String home() {
+//        return "Hello World!";
+//    }
+
+//   unikamy pisania html w controllerach
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String home() {
+//        return """
+//                <html>
+//                <head>
+//                    <title>Home Page</title>
+//                </head>
+//                <body>
+//                    <h1>Home Page</h1>
+//                    <p>Hello World!</p>
+//                </body>
+//                <html>
+//                """;
+//    }
+
+}
