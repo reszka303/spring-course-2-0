d791f3c6fb85cf70775309e2cabb4e0f9052dcad
reszka303 <reszka303@op.pl>
2021-12-14 09:20:39 +0100

    49th commit: Thymeleaf - Fragments and Layouts - Layouts

---
+++ _46-layouts/src/main/java/pl/javastart/layouts/LayoutsApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart.layouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutsApplication.class, args);
+    }
+}
---
+++ _46-layouts/src/main/java/pl/javastart/layouts/ProductController.java
@@ -0,0 +1,33 @@
+package pl.javastart.layouts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/")
+    String home(Model model) {
+        List<Product> allProducts = productRepository.findAll();
+        model.addAttribute("products", allProducts);
+        return "index";
+    }
+
+    @GetMapping("/product")
+    String getProduct(@RequestParam Long id, Model model) {
+        Optional<Product> product = productRepository.findById(id);
+        product.ifPresent(value -> model.addAttribute("product", value));
+        return "product";
+    }
+}
