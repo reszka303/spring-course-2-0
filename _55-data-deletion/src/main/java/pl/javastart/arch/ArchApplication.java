efe4b863122552bc8447119b6976dcc441d48f88
reszka303 <reszka303@op.pl>
2021-12-27 23:20:40 +0100

    59th commit: API - Application Architecture and CRUD - Data deletion

---
+++ _55-data-deletion/src/main/java/pl/javastart/arch/ArchApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchApplication.class, args);
+    }
+}
