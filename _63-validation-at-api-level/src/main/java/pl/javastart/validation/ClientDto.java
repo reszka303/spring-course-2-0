e7a83e12e99339232f474a88c394f4d6e44ce5e6
reszka303 <reszka303@op.pl>
2022-01-04 16:34:57 +0100

    68th commit: Validation - Integration - Validation at API level

---
+++ _63-validation-at-api-level/src/main/java/pl/javastart/validation/ClientDto.java
@@ -0,0 +1,54 @@
+package pl.javastart.validation;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+class ClientDto {
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String firstName;
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String lastName;
+    @NotNull
+    @Email
+    @Size(min = 5, max = 100)
+    private String email;
+    @Min(value = 1)
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ _63-validation-at-api-level/src/main/java/pl/javastart/validation/ClientRepository.java
@@ -0,0 +1,9 @@
+package pl.javastart.validation;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+    boolean existsByUsername(String username);
+
+}
